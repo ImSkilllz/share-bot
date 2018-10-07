0feeb3a6aadc7754c7e2e60d75bb206db5d2c670
ImSkilllz <43930333+ImSkilllz@users.noreply.github.com>
2018-10-07 16:29:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,17 @@
-﻿const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
 const Discord = require('discord.js');
-const client = new Discord.Client();
- bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`Hey, Wassup!`);
-});
-console.log("Welcome Again !");
+const A7MD = new Discord.Client();
+
+console.log("BOT ONLINE");
  
- client.on("guildMemberRemove", member => {
+A7MD.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
 **
-:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
-╔╦╦╦═╦╗╔═╦═╦══╦═╗
-║║║║╩╣╚╣═╣║║║║║╩╣
-╚══╩═╩═╩═╩═╩╩╩╩═╝ 
- bot.on("message", async message => {
-‏نقدم لك دعوة لسموك الراقيَ .
-‏لنحظى بجمال حضورك وتواجدك
-•بتنـورونـا الجميع :wink:
-•اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:
-•عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:
-•واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
-شرفنا يا عسل:wink: :kissing_heart:
-   if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
-https://discord.gg/CnTFFV4
-joined info-selfie
-! Selfie.                              
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-**`)
-}).catch(console.error)
-   let prefix = '-';
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-})
- 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
-╔╦╦╦═╦╗╔═╦═╦══╦═╗
-║║║║╩╣╚╣═╣║║║║║╩╣
-╚══╩═╩═╩═╩═╩╩╩╩═╝ 
-   if (cmd === `${prefix}ping`){
-    message.channel.send("Pong!");
-  }
-});
-‏نقدم لك دعوة لسموك الراقيَ .
-‏لنحظى بجمال حضورك وتواجدك
-•بتنـورونـا الجميع :wink:
-•اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:
-•عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:
-•واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
-شرفنا يا عسل:wink: :kissing_heart:
- bot.login(process.env.token);
-https://discord.gg/CnTFFV4
-joined info-selfie
-! Selfie.                              
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+سلام عليكم 
+لو سمحت ممكن تدخل سيرفرى
+                               [ https://discord.gg/CnTFFV4 ] 
+الدعووة خاصة لك ... [ ${member}  ]
 **`) 
 }).catch(console.error)
 })
-client.login("NDc0Mjk5OTE0NDg3MDcwNzIx.Dpqm2A.HoR9poko_WQt2GJHrU-AnR7Y_4M");
+A7MD.login("NDc0Mjk5OTE0NDg3MDcwNzIx.Dpqm2A.HoR9poko_WQt2GJHrU-AnR7Y_4M");
