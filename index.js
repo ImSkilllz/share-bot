7263f254c981c596fd9ea5b857a9c904aa0afda3
ImSkilllz <43930333+ImSkilllz@users.noreply.github.com>
2018-10-11 20:34:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ A7MD.on("guildMemberAdd", member => {
 **
 سلام عليكم 
 10 invite = 40k credit
-                               [ https://discord.gg/xybcgv ] 
+                               [ https://discord.gg/ZJzXeqD ] 
 الدعووة خاصة لك ... [ ${member}  ]
 **`) 
 }).catch(console.error)
