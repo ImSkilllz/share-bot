b9251a5c43c89a1b7afefc7819dfb134e093b93a
ImSkilllz <43930333+ImSkilllz@users.noreply.github.com>
2018-10-13 01:21:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const A7MD = new Discord.Client();
 
-console.log("Welcome SPECIAL !");
+console.log("BOT ONLINE");
  
- client.on("guildMemberRemove", member => {
+A7MD.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
-**سلام عليكم
-10 invite = 25 credits !!!
-[ https://discord.gg/2Tq6HFJ ] 
-الدعوة خاصة لك ي قلبي... [ ${member}  ]
-**`)
-}).catch(console.error)
-
-})
- 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**سلام عليكم
-افضل سيرفر عربي ل اكواد بوتات دسكورد
-                               [ https://discord.gg/SqETYQy ] 
-الدعوة خاصة لك ي قلبي... [ ${member}  ]
+**
+سلام عليكم 
+10 invite = 25k credit !!
+[ https://discord.gg/SqETYQy ] 
+الدعووة خاصة لك ... [ ${member}  ]
 **`) 
 }).catch(console.error)
 })
-client.login("NDc0MzI1NTc0NjQ3NTQ1ODg3.DqKsZw.nqXx-BV9lkcYrYg1GTOBf7-C9-s");
+A7MD.login("NDc0MzI1NTc0NjQ3NTQ1ODg3.DqKsZw.nqXx-BV9lkcYrYg1GTOBf7-C9-s");
