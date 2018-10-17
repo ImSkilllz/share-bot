0f8000479d85a402f8fe11d8108a386b5c9f699d
ImSkilllz <43930333+ImSkilllz@users.noreply.github.com>
2018-10-17 21:36:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,17 +9,9 @@ client.on('ready', () => {
 client.on('guildMemberAdd', async event => {
 
   let message = '**\n`15 invite = 40k credit لحق! \n>> https://discord.gg/CaMm858`**';
-  let message2 = 'discord.gg/testt';
-  let message3 = 'discord.gg/test5';
   setTimeout(function(){
-    event.send(message);
+    event.member.send(message);
   }, ms("10s"));
-  setTimeout(function(){
-    event.send(message2);
-  }, ms("20s"));
-  setTimeout(function(){
-    event.send(message3);
-  }, ms("30s"));
 });
 
-client.login('NDc0MzA3NzgzMjk0OTc2MDAw.DqfjwA.TbCWSHjSWScI3HL0canNeAT07EE');
+client.login('NDc4NTMxNzAyMjY0OTU0ODgx.DqkR9A.aJnIRUpzWLW-M-d-oMk0lIfVBqI');
